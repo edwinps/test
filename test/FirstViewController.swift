25b02363bac398f6641b49b88a6cb14d275ebee1
Edwin peña <edwin-jose.pena@soprasteria.com>
2020-01-13 10:03:07 +0100

    add base project

---
+++ test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  test
+//
+//  Created by PENA SANCHEZ Edwin Jose on 13/01/2020.
+//  Copyright © 2020 safe365. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  test
+//
+//  Created by PENA SANCHEZ Edwin Jose on 13/01/2020.
+//  Copyright © 2020 safe365. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
